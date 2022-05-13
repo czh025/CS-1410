472270f1a818682829e2d54484f910ec14638f12
Czh025 <izhihuichen@gmail.com>
2022-05-13 01:17:30 -0600

    t

--- Project 1 - Book Recommendations/bookrecs.py
+++ Project 1 - Book Recommendations/bookrecs.py
@@ -8,24 +8,30 @@ Course: CS1410-X01
 
 def main():
     """
-    TODO
+    Getting necessary data
+    name: string
+    people: dict
     """
-    name = input("Enter a reader's name: ")
+    name = input("Enter a reader's name: \u001b[1m").lower()  # albus dumbledore
     people = get_data()
-    if name.lower() not in people:
-        print(f"No such reader {name}")
-        return
+    if name not in people:
+        return print(f"\u001b[0mNo such reader {name}")
     friends_lst = friends(name, people)
-    print(f"Recommendations for {name} from {friends_lst[0]} and {friends_lst[1]}:")
-    recommend(people[name], {friends_lst[0]: people[friends_lst[0]], friends_lst[1]: people[friends_lst[1]]})
+    recommend(people[name], [people[friends_lst[0]], people[friends_lst[1]]])
 
 
 def friends(name, people):
     """
-    TODO
+    name: string, which user entered
+    people: dict
+
+    find the two people with the highest affinity scores
+
+    return a list with two names
     """
     affinity_score = {}
     for person in people:
+        # filter the name which user entered
         if person != name:
             for index, rate in enumerate(people[person]):
                 if person not in affinity_score:
@@ -34,24 +40,34 @@ def friends(name, people):
     affinity_score = dict(sorted(affinity_score.items(), key=lambda item: -item[1]))
     val = [i for i in affinity_score.keys()][0: 2]
     val.sort()
+    print(f"\u001b[0mRecommendations for {name} from {val[0]} and {val[1]}:")
     return val
 
 
 def recommend(target_person, people):
     """
-    TODO
+    target_person: list, the scores for comparing
+    people: list in list, the scores for comparing to target_person
+
+    recommend_book_lst uses to avoid print the same book title
     """
     recommend_book_lst = []
     with open("booklist.txt", "r") as booklist_file:
         booklist_file_lines = booklist_file.read().splitlines()
         for person in people:
-            for index, data in enumerate(people[person]):
+            for index, data in enumerate(person):
                 if data >= 3 and target_person[index] == 0 and booklist_file_lines[index] not in recommend_book_lst:
                     recommend_book_lst.append(booklist_file_lines[index])
-                    print("     ", booklist_file_lines[index])
+                    # TODO sort by first name, last name, then title
+                    lst = booklist_file_lines[index].split(",")
+                    print(f"      {lst[0]},\t{lst[1]}")
 
 
 def get_data():
+    """
+    Get necessary data from 'ratings.txt' file
+    return a dict
+    """
     people = {}
     with open("ratings.txt", "r") as rating_file:
         rating_file_lines = rating_file.read().splitlines()
--- test.py
+++ test.py
@@ -1,20 +1,7 @@
 def main():
-    my_str = "hello"
-    vowels = ["a", "e", "i", "o", "u"]
-    print("".join(["*" if x in vowels else x for x in my_str]))
-    # new_str = my_str.replace(i for i in my_str if i in vowels, "*")
-    # print(t(2, lst))
-    # print((lambda x: x if x in my_str else "")(i for i in ["a", "e", "i", "o", "u"]))
-    print
+    dic = {"a": 5, "b": 3, "c": 4, "d": 1}
+    print(dic.values())
 
-def most_frequent(word):
-  dic = {}
-  for char in word:
-    if char not in dic:
-      dic[char] = 1
-    else:
-      dic[char] += 1
-  print(dic)
+
 if __name__ == '__main__':
-    most_frequent("hello")
-    # main()
+    main()
