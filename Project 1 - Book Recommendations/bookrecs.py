80317d7812edfe3d5dd2df1789052a99e9ef85c9
Czh025 <izhihuichen@gmail.com>
2022-05-13 19:31:08 -0600

    Finish First project

--- Project 1 - Book Recommendations/bookrecs.py
+++ Project 1 - Book Recommendations/bookrecs.py
@@ -3,6 +3,10 @@ Project Name: Book Recommendations
 Author: Zhihui Chen
 Due Date: 05/18/2022
 Course: CS1410-X01
+
+This project can suggest book that users might like based on the books they like
+
+Before running this project make sure you have the 'booklist.txt' and 'ratings.txt' files.
 """
 
 
@@ -12,18 +16,24 @@ def main():
     name: string
     people: dict
     """
+    # \u001b[1m add bold text style
     name = input("Enter a reader's name: \u001b[1m").lower()  # albus dumbledore
+    # \u001b[0m restore to default style
+    print('\u001b[0m', end='')
     people = get_data()
     if name not in people:
-        return print(f"\u001b[0mNo such reader {name}")
+        return print(f"No such reader {name}")
     friends_lst = friends(name, people)
-    recommend(people[name], [people[friends_lst[0]], people[friends_lst[1]]])
+    print(f"Recommendations for {name} from {friends_lst[0]} and {friends_lst[1]}:")
+    rec_book_lst = recommend(people[name], [people[friends_lst[0]], people[friends_lst[1]]])
+    for i in rec_book_lst:
+        print("     ", "\t".join(i))
 
 
 def friends(name, people):
     """
     name: string, which user entered
-    people: dict
+    people: dict, data from booklist.txt
 
     find the two people with the highest affinity scores
 
@@ -38,9 +48,7 @@ def friends(name, people):
                     affinity_score[person] = 0
                 affinity_score[person] += rate * people[name][index]
     affinity_score = dict(sorted(affinity_score.items(), key=lambda item: -item[1]))
-    val = [i for i in affinity_score.keys()][0: 2]
-    val.sort()
-    print(f"\u001b[0mRecommendations for {name} from {val[0]} and {val[1]}:")
+    val = sorted([*affinity_score][0: 2])
     return val
 
 
@@ -49,18 +57,19 @@ def recommend(target_person, people):
     target_person: list, the scores for comparing
     people: list in list, the scores for comparing to target_person
 
-    recommend_book_lst uses to avoid print the same book title
+    rec_book_lst uses to avoid print the same book title
+
+    return a list with recommend book data
     """
-    recommend_book_lst = []
+    rec_book_lst = []
     with open("booklist.txt", "r") as booklist_file:
-        booklist_file_lines = booklist_file.read().splitlines()
+        book_lines = booklist_file.read().splitlines()
         for person in people:
             for index, data in enumerate(person):
-                if data >= 3 and target_person[index] == 0 and booklist_file_lines[index] not in recommend_book_lst:
-                    recommend_book_lst.append(booklist_file_lines[index])
-                    # TODO sort by first name, last name, then title
-                    lst = booklist_file_lines[index].split(",")
-                    print(f"      {lst[0]},\t{lst[1]}")
+                if data >= 3 and target_person[index] == 0 and book_lines[index].split(",") not in rec_book_lst:
+                    rec_book_lst.append(book_lines[index].split(","))
+    rec_book_lst = sorted(rec_book_lst, key=lambda x: (x[0].split()[-1], x[0].split()[0], x[-1]))
+    return rec_book_lst
 
 
 def get_data():
