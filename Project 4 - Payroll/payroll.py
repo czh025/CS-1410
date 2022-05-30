feb8ed1ae6f33f6bb4267d6625529e0715ed2350
Czh025 <izhihuichen@gmail.com>
2022-05-29 20:31:26 -0600

    Fourth almost done

--- Project 4 - Payroll/payroll.py
+++ Project 4 - Payroll/payroll.py
@@ -22,18 +22,18 @@ def find_employee_by_id(emp_id):
     for emp in employees:
         if emp.emp_id == emp_id:
             return emp
-    raise Exception("No such an employees.")
+    raise Exception("No such an employee.")
 
 
 class Employee:
     def __init__(self, emp_id, first_name, last_name, address, city, state, zipcode, classification):
         self.emp_id = emp_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__address = address
+        self.__city = city
+        self.__state = state
+        self.__zipcode = zipcode
         self.classification = classification
 
     def make_salaried(self, salary):
@@ -47,7 +47,10 @@ class Employee:
 
     def issue_payment(self):
         salary = self.classification.compute_pay()
-#         TODO
+        with open(PAY_LOGFILE, "a") as pay_log_f:
+            if salary > 0:
+                pay_log_f.write(f"Mailing {salary:.2f} to {self.__first_name} {self.__last_name} "
+                                f"at {self.__address} {self.__city} {self.__state} {self.__zipcode}\n")
 
 
 class Classification(ABC):
@@ -66,37 +69,37 @@ class Salaried(Classification):
 
 class Hourly(Classification):
     def __init__(self, hour_rate):
-        self.hour_rate = hour_rate
-        self.timecards = []
+        self.__hour_rate = hour_rate
+        self.__timecards = []
 
     def compute_pay(self):
-        salary = round(sum(self.timecards) * self.hour_rate, 2)
-        self.timecards.clear()
+        salary = round(sum(self.__timecards) * self.__hour_rate, 2)
+        self.__timecards.clear()
         return salary
 
     def add_timecard(self, hours):
-        self.timecards.append(hours)
+        self.__timecards.append(hours)
 
 
 class Commissioned(Salaried):
     def __init__(self, salary, commission_rate):
         super().__init__(salary)
-        self.commission_rate = commission_rate
-        self.receipts = []
+        self.__commission_rate = commission_rate
+        self.__receipts = []
 
     def compute_pay(self):
-        salary = round(self.salary / 24 + sum(self.receipts) * self.commission_rate / 100, 2)
-        self.receipts.clear()
+        salary = round(self.salary / 24 + sum(self.__receipts) * self.__commission_rate / 100, 2)
+        self.__receipts.clear()
         return salary
 
     def add_receipt(self, receipt):
-        self.receipts.append(receipt)
+        self.__receipts.append(receipt)
 
 
 def load_employees():
     """
     EMPLOYEE_FILE's info:
-    id, first_name, last_name, address, city, state, zipcode, classification, salary, commission, hourly
+    id, __first_name, __last_name, __address, __city, __state, __zipcode, classification, salary, commission, hourly
 
     classification 1: salary
     classification 2: commission
@@ -106,29 +109,37 @@ def load_employees():
         emp_f.readline()
         for line in emp_f:
             info = line.rstrip().split(",")
-            # emp_id = info[0]
-            # first_name = info[1]
-            # last_name = info[2]
-            # addr = info[3]
-            # city = info[4]
-            # state = info[5]
-            # zipcode = info[6]
             classification = int(info[7])
             salary = float(info[8])
             commission = float(info[9])
             hourly = float(info[10])
             employee_classification = Salaried(salary) if classification == 1 else \
                 Commissioned(salary, commission) if classification == 2 else Hourly(hourly)
-            # employees.append(Employee(emp_id, first_name, last_name, addr, city, state, zipcode, classification))
             employees.append(Employee(*info[:7], employee_classification))
 
 
 def process_timecards():
-    pass
+    with open(TIMECARDS_FILE, "r") as timecard_f:
+        for line in timecard_f:
+            info = line.strip().split(",")
+            emp_id = info[0]
+            timecards = list(map(float, info[1:]))
+            employee = find_employee_by_id(emp_id)
+            if isinstance(employee.classification, Hourly):
+                for card in timecards:
+                    employee.classification.add_timecard(card)
 
 
 def process_receipts():
-    pass
+    with open(RECEIPTS_FILE, "r") as receipts_f:
+        for line in receipts_f:
+            info = line.strip().split(",")
+            emp_id = info[0]
+            receipts = list(map(float, info[1:]))
+            employee = find_employee_by_id(emp_id)
+            if isinstance(employee.classification, Commissioned):
+                for receipt in receipts:
+                    employee.classification.add_receipt(receipt)
 
 
 def run_payroll():
