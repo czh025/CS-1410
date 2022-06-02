e2485bd3e5163b7898d4945a32ac7f1627506fef
Czh025 <izhihuichen@gmail.com>
2022-06-02 16:02:14 -0600

    finish fifth

--- Project 5 - Data Visualization/dataplot.py
+++ Project 5 - Data Visualization/dataplot.py
@@ -4,20 +4,28 @@ Author: Zhihui Chen
 Due Date: 06/17/2022
 Course: CS1410-X01
 
-Put your description here, lessons learned here, and any other information someone using your
-program would need to know to make it run.
+This program can read all .dat file from current directory,
+    finally output an .out file and a .pdf file
+    out file shows the location where the pulse occurred
+        and the area of the pulse from original data
+    pdf file shows both original data and processed data figure
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
 import glob
 import sys
+import numpy as np
+import matplotlib.pyplot as plt  # pylint: disable=import-error
 
 VT = 100
 PULSE_NUMBER = 50
 
 
 def find_pulses(data, pulses, index=0):
+    """
+    use the recursion to find the data where is great then VT, save the index to pulses,
+    then skip the continuous rising data until a descending data appears, starts the next recursion
+    return list
+    """
     if index >= len(data) - 2:
         return pulses
     if data[index + 2] - data[index] > VT:
@@ -29,6 +37,11 @@ def find_pulses(data, pulses, index=0):
 
 
 def process_data(raw_data):
+    """
+    Make the raw data into smoothed data
+    finally add the first three and last three data from the raw data into the smoothed data
+    return list
+    """
     data = [int((raw_data[i - 3] + 2 * raw_data[i - 2] + 3 * raw_data[i - 1] +
                  3 * raw_data[i] + 3 * raw_data[i + 1] + 2 * raw_data[i + 2] +
                  raw_data[i + 3]) // 15)
@@ -39,13 +52,24 @@ def process_data(raw_data):
 
 
 def analyze(f_name):
+    """
+    read the raw data from f_name
+    process raw data into smoothed data
+    save data into a pdf file
+    save data into a out file
+    """
     raw_data = np.loadtxt(f_name)
-    data = process_data(raw_data)
-    save_to_pdf(raw_data, data, f_name)
-    save_to_out(raw_data, data, f_name)
+    smooth_data = process_data(raw_data)
+    save_to_pdf(raw_data, smooth_data, f_name)
+    save_to_out(raw_data, smooth_data, f_name)
 
 
 def save_to_pdf(raw_data, smooth_data, file_name):
+    """
+    the pdf shows both raw data and smooth data,
+    the raw data's x-axis is invisible
+    finally save the figure to dpf file
+    """
     fig, axs = plt.subplots(2)
 
     axs[0].plot(raw_data, lw=.2)
@@ -60,24 +84,28 @@ def save_to_pdf(raw_data, smooth_data, file_name):
 
 
 def save_to_out(raw_data, smooth_data, file_name):
+    """
+    use smooth data to find the range, then use the range to retrieve the data from raw data
+    pulse range up to 50, if the value in pulses list is not last one, it may end early
+    """
     pulses = find_pulses(smooth_data, [], 0)
     with open(f"{file_name[:-4]}.out", "w", encoding="utf-8") as write_f:
         write_f.write(f"{file_name}:\n")
-        for i in range(len(pulses)):
-            start_pulse = pulses[i]
+        for index, val in enumerate(pulses):
             pulse_range = PULSE_NUMBER
             # avoid double counting of area
-            if i < len(pulses) - 1 and pulses[i] + pulse_range > pulses[i + 1]:
-                pulse_range = pulses[i + 1] - start_pulse
-            pulse_range = min(pulse_range, len(smooth_data) - start_pulse)
-            area = int(sum(raw_data[start_pulse:start_pulse + pulse_range]))
-            write_f.write(f"Pulse {i + 1}: {start_pulse} ({area})\n")
+            if index < len(pulses) - 1 and val + pulse_range > pulses[index + 1]:
+                pulse_range = pulses[index + 1] - val
+            pulse_range = min(pulse_range, len(smooth_data) - val)
+            area = int(sum(raw_data[val:val + pulse_range]))
+            write_f.write(f"Pulse {index + 1}: {val} ({area})\n")
+
 
 def main():
     """
-    Program starts here.
+    set maximum recursion limit to 10000
+    find all files with the dat extension in the current directory
     """
-    # set maximum recursion limit to 10000
     sys.setrecursionlimit(10000)
     for f_name in glob.glob("*dat"):
         analyze(f_name)
