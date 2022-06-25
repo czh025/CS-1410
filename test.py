34a69e3a66ea539c18bc00ce2e5676b0106d1e1c
czh025 <izhihuichen@gmail.com>
2022-06-24 20:10:32 -0600

    test

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
-"""
-metro_lat_long.py
-
-Demonstration of nested tuple unpacking::
-
-    >>> main()
-                    |  latitude | longitude
-    Mexico City     |   19.4333 |  -99.1333
-    New York-Newark |   40.8086 |  -74.0204
-    São Paulo       |  -23.5478 |  -46.6358
-
-"""
-
-# tag::MAIN[]
-phone = ["3852075000", "15980101010"]
-
 def main():
-    for record in phone:
-        match tuple(record):  # <1>
-            case ["1", *rest]:  # <2>
-                print("China", rest)
-            case ["3", *rest]:
-                print("America", rest)
-# end::MAIN[]
+    print("test")
+
 
 if __name__ == '__main__':
     main()
